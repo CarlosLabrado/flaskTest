b551af1aa66ac12b69d0de3bb6e995f25cf1e2e1
CarlosLabrado <carlos@petrolog.us>
2018-04-05 13:16:20 -0600

    TEST: zerorpc test, apparently there's already a single network and both containers are in it, so they should be reachable by name

--- frontend/front.py
+++ frontend/front.py
@@ -32,7 +32,7 @@ def hello():
             import zerorpc
 
             c = zerorpc.Client()
-            c.connect("tcp://127.0.0.1:4242")
+            c.connect("tcp://data:4242")
             print(c.add_man("carlos"))
         else:
             print(form.errors)
