c1ab41312cda9b10d946d16804134f26db9daec0
CarlosLabrado <carlos@petrolog.us>
2018-05-03 16:17:25 -0600

    Trying: https://github.com/resin-io-playground/access-point-example/blob/master/start.sh

--- frontend/front.sh
+++ frontend/front.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
-
+# Disable Network Manager wireless
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-nmcli connection up petrologap
+dbus-send --system --print-reply --dest=org.freedesktop.NetworkManager /org/freedesktop/NetworkManager org.freedesktop.DBus.Properties.Set string:"org.freedesktop.NetworkManager" string:"WirelessEnabled" variant:boolean:false
+
+# Do some cleaning up
+pkill -9 dnsmasq
+rfkill unblock wlan
+ifdown wlan0
+ifup wlan0
+
+# Start the access point
+hostapd /etc/hostapd/hostapd.conf &
+dnsmasq --keep-in-foreground
 
 echo ..... start.sh: Running main app ....
 python /usr/src/app/front.py
