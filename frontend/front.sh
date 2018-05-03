bbaf6d919b514f6c17984d6241777dcdd8376bb6
CarlosLabrado <carlos@petrolog.us>
2018-05-03 15:20:07 -0600

    TEST: lets move from sh

--- frontend/front.sh
+++ frontend/front.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-echo try to run ap
-
-# 2. Is there Internet connectivity?
-nmcli -t g | grep full
-
-nmcli connection up petrologap
-echo running ap
-
 echo ..... start.sh: Running main app ....
 python /usr/src/app/front.py
