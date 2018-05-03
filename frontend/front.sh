fc9fb85f3afad15c7519a7ac454834117192e6b1
CarlosLabrado <carlos@petrolog.us>
2018-05-03 15:34:50 -0600

    TEST: try the wrapper from : https://github.com/resin-io-playground/resin-nmcli/blob/master/Dockerfile.template

--- frontend/front.sh
+++ frontend/front.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#! /bin/bash
+
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+nmcli --nocheck "$@"
 
 echo ..... start.sh: Running main app ....
 python /usr/src/app/front.py
