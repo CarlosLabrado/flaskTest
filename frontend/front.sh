052246366b5294e922a4aba7b7cc4fec9e864583
CarlosLabrado <carlos@petrolog.us>
2018-05-03 16:30:43 -0600

    Test: last one was too hacky this must be the proper way

--- frontend/front.sh
+++ frontend/front.sh
@@ -1,17 +1,9 @@
 #! /bin/bash
 # Disable Network Manager wireless
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-dbus-send --system --print-reply --dest=org.freedesktop.NetworkManager /org/freedesktop/NetworkManager org.freedesktop.DBus.Properties.Set string:"org.freedesktop.NetworkManager" string:"WirelessEnabled" variant:boolean:false
 
-# Do some cleaning up
-pkill -9 dnsmasq
-rfkill unblock wlan
-ifdown wlan0
-ifup wlan0
-
-# Start the access point
-hostapd /etc/hostapd/hostapd.conf &
-dnsmasq --keep-in-foreground
+echo activate connection first
+python /usr/src/app/activate_connection.py
 
 echo ..... start.sh: Running main app ....
 python /usr/src/app/front.py
