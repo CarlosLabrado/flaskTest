578a5f871ee11dd24a2862aaea1d194795de1a7e
CarlosLabrado <carlos@petrolog.us>
2018-05-04 11:00:39 -0600

    Test: there's still a problem with the export

--- frontend/front.sh
+++ frontend/front.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-# Disable Network Manager wireless
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+dbus-send --system --print-reply --dest=org.freedesktop.NetworkManager /org/freedesktop/NetworkManager org.freedesktop.DBus.Properties.Set string:"org.freedesktop.NetworkManager" string:"WirelessEnabled" variant:boolean:false
 
 echo activate connection first
 python /usr/src/app/activate_connection.py
