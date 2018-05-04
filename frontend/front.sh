fed61acd6d0e53f56a8b549ed7a004356dd67ada
CarlosLabrado <carlos@petrolog.us>
2018-05-04 14:45:08 -0600

    Test: there's still a problem with the export

--- frontend/front.sh
+++ frontend/front.sh
@@ -1,6 +1,7 @@
-#! /bin/bash
+#!/bin/bash
+
+echo trying to export address
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-dbus-send --system --print-reply --dest=org.freedesktop.NetworkManager /org/freedesktop/NetworkManager org.freedesktop.DBus.Properties.Set string:"org.freedesktop.NetworkManager" string:"WirelessEnabled" variant:boolean:false
 
 echo activate connection first
 python /usr/src/app/activate_connection.py
