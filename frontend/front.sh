8123448992902e172405abb50386430b79922e78
CarlosLabrado <carlos@petrolog.us>
2018-05-14 12:22:52 -0600

    Test: we now have 3 containers, one of them has the wifi-connect and we don't even want that working, but it allows us to have ip and DHCP on our clients

--- frontend/front.sh
+++ frontend/front.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-#start wifi connect
-echo starting wifi connect
-./wifi-connect --portal-listening-port 45454
-
 #start main app
 echo .... Running main app ....
 python /usr/src/app/front.py
